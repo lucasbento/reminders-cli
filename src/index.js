845a11fc5dc289718d64334191e15cbb4c0e8629
Lucas Bento <lucas.bsilva@outlook.com>
2017-08-24 12:23:30 +0200

    Add yargs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import 'babel-polyfill';
-import runApplescript from 'run-applescript';
+
+import yargs, { argv } from 'yargs';
 import applescript from 'applescript';
 
-const handleReminder = async () => {
+yargs.usage('Command-line utility to handle reminders on Mac OSX.', {
+  'list': {
+    description: 'List all reminders that are not completed.',
+    short: 'l',
+  },
+  'add': {
+    description: 'Create a new reminder.',
+    requiresArg: true,
+    short: 'a'
+  }
+});
+
+const showReminderList = async () => {
   applescript.execFile(`${__dirname}/scripts/get_reminders.applescript`, function(err, rtn) {
     if (err) {
       console.log(err);
@@ -12,4 +25,12 @@ const handleReminder = async () => {
   });
 };
 
-handleReminder();
+const run = () => {
+  if (argv.list || argv.l) {
+    return showReminderList();
+  }
+
+  yargs.showHelp();
+};
+
+run();

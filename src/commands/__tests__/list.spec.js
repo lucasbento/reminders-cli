ebef1c21f506595b8fafbe66ef960d18000674c5
Lucas Bento <lucas.bsilva@outlook.com>
2017-09-04 14:17:47 +0200

    Update tests

--- src/commands/__tests__/list.spec.js
+++ src/commands/__tests__/list.spec.js
@@ -13,10 +13,14 @@ beforeEach(async () => {
   await addReminder();
 });
 
+// TODO: provide commands to inquirerjs to update the reminder
+
 it('should handle the list command', async () => {
   await list();
 
   const prompts = inquirer.getPrompts();
+
+  const reminderDate = moment().add(NUMBER_DAYS_REMINDER_DATE, 'days').format('DD/MM/YYYY');
 
   expect(prompts[0].type).toEqual('list');
   expect(prompts[0].name).toEqual('name');
@@ -26,8 +30,8 @@ it('should handle the list command', async () => {
   expect(prompts[1].default).toEqual(REMINDER_NAME);
 
   expect(prompts[2].name).toEqual('date');
-  expect(prompts[2].default).toEqual(moment().add(NUMBER_DAYS_REMINDER_DATE, 'days').format('DD/MM/YYYY'));
+  expect(prompts[2].choices).toEqual(expect.arrayContaining([`Current (${reminderDate})`]));
 
-  expect(prompts[3].name).toEqual('time');
-  expect(prompts[3].default).toEqual(`${REMINDER_HOUR}:00`);
+  expect(prompts[4].name).toEqual('time');
+  expect(prompts[4].default).toEqual(`${REMINDER_HOUR}:00`);
 });

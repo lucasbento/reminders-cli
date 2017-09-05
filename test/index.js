97b27916cbd061f7cb0cb0d42da56762fb395a80
Lucas Bento <lucas.bsilva@outlook.com>
2017-09-05 16:19:29 +0200

    Increase the test timeout interval for travis

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ import { clearReminders } from './helper';
 import inquirer from '../src/__mocks__/inquirer';
 
 // eslint-disable-next-line no-undef
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 40000;
 
 if (process.env.NODE_ENV === 'CI') {
   console.log = () => {};
